78a5c4109cdfd0043e8bb8fd89e72e1097c4f54e
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-09-15 17:02:20 +0100

    Created MongoDB database and connected it to the server

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,26 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const app = express();
 
 const PORT = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
 
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB Atlas:', error);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
