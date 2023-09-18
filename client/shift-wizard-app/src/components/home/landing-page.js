455f8628192e32c0a293e5b436b96db93c571653
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-09-18 09:28:24 +0100

    images added on landing page but not fully responsive yet

--- client/shift-wizard-app/src/components/home/landing-page.js
+++ client/shift-wizard-app/src/components/home/landing-page.js
@@ -1,10 +1,33 @@
-import ShiWiLogo from '../../assets/ShiWiLogo2.png';
+import wizardHat from '../../assets/wizardHat.png';
+import magic from '../../assets/magic.png';
 import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
     <div className='bg-background1 relative h-screen w-full mx-auto flex items-center justify-center'>
-        <div className='text-center pt-[180px]'>
+        <div className='flex items-center justify-center'>
+        <img
+          src={wizardHat}
+          alt='wizard hat'
+          className='absolute w-[200px] top-[-40px] right-[5%]
+                    sm:w-[320px] sm:right-[23%]
+                    md:w-[380px] md:right-[15%]
+                    lg:w-[460px] lg:right-[5%] lg:top-[-120px]
+                    xl:w-[520px] xl:right-[25%]
+                    z-1'
+        />
+        <img
+          src={magic}
+          alt='magic'
+          className='absolute w-[400px] top-[80px] right-[10%]
+                    sm:w-[600px] sm:right-[7%] -rotate-45
+                    md:w-[700px] md:right-[20%] rotate-0
+                    lg:w-[800px] lg:right-[25%] rotate-0
+                    xl:w-[1000px] xl:right-[30%] rotate-0
+                    z-0'
+        />
+        </div>
+        <div className='text-center top-[140px] z-10'>
           <h1 className='text-primaryT font-title font-bold
                         text-[80px] sm:text-[100px] md:text-[140px] lg:text-[180px]'
                         style={{ textShadow: '2px 2px 10px rgba(0, 0, 0, 5)' }}
