ae925b13b4f98fe90b9cb3e5db4455e0cd31a5c3
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-09-27 08:19:00 +0100

    added Register component

--- client/shift-wizard-app/src/components/users/auth.js
+++ client/shift-wizard-app/src/components/users/auth.js
@@ -1,13 +1,56 @@
 import Footer from '../footer/footer';
-import Navbar from '../navbar/navbar';
+import { useState } from 'react';
 
 function Auth () {
     return (
       <div className="auth">
-
+        <Login />
+        <Register />
         <Footer />
       </div>
-    )
-  }
+   );
+};
   
-export default Auth;
+function Login () {
+    return (
+      <div className="login">
+        <h1>Login</h1>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    );
+};
+
+function Register () {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+      <div className="register-container ">
+        <form>
+          <h2>Register</h2>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              placeholder="Username"
+              id="username"
+            />
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="Password"
+              id="password"
+            />
+          </div>
+          <button type="submit">Register</button> 
+        </form>
+      </div>
+    );
+}
