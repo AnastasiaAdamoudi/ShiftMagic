6e25cc1c6d24eb7485d3f2cb0876c110fa21c0d4
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-09-27 23:57:05 +0100

    added 'generate shifts' tab at user home

--- client/shift-wizard-app/src/components/users/user-home.js
+++ client/shift-wizard-app/src/components/users/user-home.js
@@ -3,23 +3,30 @@ import Footer from "../footer/footer";
 import { Link } from 'react-router-dom';
 import userSchedule from "../../assets/user-schedule.png";
 import userEmployees from "../../assets/user-employees.png";
+import userGenerate from "../../assets/user-generate.png";
 
 function UserHome() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="bg-background2 flex flex-col p-5">
-        <div className="md:flex">
-          <Link to="/employees" className="text-background1 font-title font-bold text-6xl uppercase no-underline md:w-1/2">
-            <div className="bg-background2 hover:bg-primaryT border-2 border-background1 rounded-xl p-4 relative flex flex-col items-center ml-3 mr-3 my-auto shadow-lg">
+        <div className="md:flex m-3">
+          <Link to="/employees" className="text-background1 font-title font-bold text-5xl uppercase no-underline lg:w-1/3">
+            <div className="bg-background2 hover:bg-primaryT border-2 border-background1 rounded-xl p-4 mb-4 relative flex flex-col items-center ml-3 mr-3 my-auto shadow-lg">
               <p>Employees</p>
               <img src={userEmployees} alt="employees" className="w-[200px] h-[200px]" />
             </div>
           </Link>
-          <Link to="/employees" className="text-background1 font-title font-bold text-6xl uppercase no-underline md:w-1/2">
-            <div className="bg-background2 hover:bg-primaryT border-2 border-background1 rounded-xl p-4 relative flex flex-col items-center ml-3 mr-3 my-auto shadow-lg">
+          <Link to="/employees" className="text-background1 font-title font-bold text-5xl uppercase no-underline lg:w-1/3">
+            <div className="bg-background2 hover:bg-primaryT border-2 border-background1 rounded-xl p-4 mb-4 relative flex flex-col items-center ml-3 mr-3 my-auto shadow-lg">
               <p>Shifts</p>
               <img src={userSchedule} alt="calendar" className="w-[200px] h-[200px]" />
+            </div>
+          </Link>
+          <Link to="/generate-shifts" className="text-background1 font-title font-bold text-5xl uppercase no-underline lg:w-1/3">
+            <div className="bg-background2 hover:bg-primaryT border-2 border-background1 rounded-xl p-4 relative flex flex-col items-center ml-3 mr-3 my-auto shadow-lg">
+              <p>Generate shifts</p>
+              <img src={userGenerate} alt="generate" className="w-[200px] h-[200px]" />
             </div>
           </Link>
         </div>
